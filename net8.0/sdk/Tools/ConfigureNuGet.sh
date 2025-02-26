a73684c5e24ac6c9c4569aeac336d38a2c227920
Timo Tielens-Jansen <mail@timotielens.nl>
2025-02-26 08:25:32 +0000

    use '

--- net8.0/sdk/Tools/ConfigureNuGet.sh
+++ net8.0/sdk/Tools/ConfigureNuGet.sh
@@ -3,13 +3,13 @@ Password=$2
 Source=$3
 
 echo "Adding private NuGet feed"
-echo "Username is ${Username}"
-echo "Password is ${Password"
-echo "source is ${Source"
+echo "Username is '${Username}'"
+echo "Password is '${Password'"
+echo "source is '${Source'"
 
-dotnet nuget add source "${Source}" \
+dotnet nuget add source "'${Source}'" \
       --name "privateFeed" \
-      --username "${Username}" \
-      --password "${Password}" \
+      --username "'${Username}'" \
+      --password "'${Password}'" \
       --store-password-in-clear-text; \
       dotnet nuget list source; \
