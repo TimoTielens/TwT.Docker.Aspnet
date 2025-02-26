3aabef89404dd5341eeae1885bc9eeb538f88508
Timo Tielens-Jansen <mail@timotielens.nl>
2025-02-26 08:17:32 +0000

    .

--- net8.0/sdk/Tools/ConfigureNuGet.sh
+++ net8.0/sdk/Tools/ConfigureNuGet.sh
@@ -3,9 +3,9 @@ echo "Username is '$1'"
 echo "Password is '$2'"
 echo "source is '$3'"
 
-dotnet nuget add source "'$3'" \
+dotnet nuget add source "$3" \
       --name "privateFeed" \
-      --username "'$1'" \
-      --password "'$2'" \
+      --username "$1" \
+      --password "$2" \
       --store-password-in-clear-text; \
       dotnet nuget list source; \
