83b5fb91880cba8f8d960e8d031cee3f69ff8f91
Timo Tielens-Jansen <mail@timotielens.nl>
2025-02-26 09:22:15 +0000

    is this better?

--- net8.0/sdk/Tools/ConfigureNuGet.sh
+++ net8.0/sdk/Tools/ConfigureNuGet.sh
@@ -7,9 +7,5 @@ echo "Username is '${Username}'"
 echo "Password is '${Password}'"
 echo "source is '${Source}'"
 
-dotnet nuget add source "'$Source'" \
-      --name "privateFeed" \
-      --username "'$Username'" \
-      --password "'$Password'" \
-      --store-password-in-clear-text; \
-      dotnet nuget list source; \
+dotnet nuget add source "$Source" --name "privateFeed" --username "$Username" --password "$Password" --store-password-in-clear-text;
+dotnet nuget list source;
