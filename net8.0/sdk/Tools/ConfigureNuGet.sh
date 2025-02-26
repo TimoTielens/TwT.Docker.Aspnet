8c84f1010422e1aefd0311f4870ff548862b50a4
Timo Tielens-Jansen <mail@timotielens.nl>
2025-02-26 07:53:59 +0000

    Add feed as well

--- net8.0/sdk/Tools/ConfigureNuGet.sh
+++ net8.0/sdk/Tools/ConfigureNuGet.sh
@@ -1,8 +1,9 @@
 echo "Adding private NuGet feed"
 echo "Username is '$1'"
 echo "Password is '$2'"
+echo "source is '$3'"
 
-dotnet nuget add source "$NUGET_FEED_URL" \
+dotnet nuget add source "$3" \
       --name "privateFeed" \
       --username "$1" \
       --password "$2" \
